dd5a9ebddd5ed37b57f5d62048087e4781195147
Adhan Razzaque <adhan.razzaque@gmail.com>
2026-01-31 13:13:05 -0600

    Remove Callbacks from flag_holders.gd

--- round/flag_holders.gd
+++ round/flag_holders.gd
@@ -8,13 +8,3 @@ var flags: int = 0b0
 
 ## The number of flag holders in this, aka the bit width
 @export_range(0, 8) var width: int = 0
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
