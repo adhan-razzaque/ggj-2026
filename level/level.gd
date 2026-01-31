d50d633b5f762d2ae200fa34bc818660175ddaa6
Adhan Razzaque <adhan.razzaque@gmail.com>
2026-01-31 16:17:44 -0600

    Initial logic for level system

--- level/level.gd
+++ level/level.gd
@@ -3,16 +3,24 @@ class_name Level extends Node
 
 # Properties
 @export_file("*.txt") var success_phrases_path: String
+@export_file("*.tscn") var next_scene_path: String
+
 
 # Variables
 var success_phrases: Array[String] = []
+var rounds: Array[Round] = []
+var current_round_index: int = -1
+@onready var timer: Timer = $Timer as Timer
+
+var current_round: Round:
+	get:
+		return rounds[current_round_index]
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	load_success_phrases()
-		
-
+	load_rounds()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -37,3 +45,50 @@ func load_success_phrases() -> void:
 		success_phrases.append(line)
 		line = file.get_line()
 	
+## Load the rounds from the children of this node
+func load_rounds() -> void:
+	var children: Array[Node] = get_children()
+	
+	for child: Node in children:
+		var round_node: Round = child as Round
+		if round_node == null:
+			continue
+		print_debug("Loading round " + round_node.name)
+		rounds.append(round_node)
+		
+## Start the next round
+func next_round() -> void:
+	if current_round_index + 1 == rounds.size():
+		end_level()
+		return
+	current_round_index += 1
+	start_round()
+	
+	
+func start_round() -> void:
+	current_round.round_over.connect(_on_round_over)
+	current_round.start_game()
+	timer.start()
+	
+		
+## End Level
+func end_level() -> void:
+	if next_scene_path.is_empty or !FileAccess.file_exists(next_scene_path):
+		return
+	get_tree().change_scene_to_file(next_scene_path)
+	
+
+## Responds to our timers timeout
+func _on_timer_timeout() -> void:
+	current_round.on_tick()
+
+func _on_round_over(won: bool) -> void:
+	timer.stop()
+	current_round.round_over.disconnect(_on_round_over)
+	
+	if won:
+		print_debug("You won, moving to next round...")
+		next_round()
+	else:
+		print_debug("You lost, try again...")
+		start_round()
